34aef36b31f7d405c3be5ee87f14e36d1ac8f5fc
indigowhale33 <motal90@hotmail.com>
2015-10-13 00:16:29 -0400

    successful xml downloading/parsing

--- app/src/main/java/com/kevinandsteve/openwindow/ResultActivity.java
+++ app/src/main/java/com/kevinandsteve/openwindow/ResultActivity.java
@@ -1,122 +1,149 @@
 package com.kevinandsteve.openwindow;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class ResultActivity extends AppCompatActivity {
+    //progress dialog
+    private ProgressDialog pDialog;
+    public static final int progress_bar_type = 0;
+    private static String xmlresponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result2);
+        EditText xmlshow = (EditText) findViewById(R.id.resulttext);
         Intent extras = getIntent();
         int zipcode = -1;
         if(extras != null){
             zipcode = extras.getIntExtra("EXTRA_ZIPCODE", 0);
         }
         String strzip = Integer.toString(zipcode);
-        Date cDate = new Date();
-        String fDate = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
-        String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=" + fDate + "&date=2015-09-27&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
-
+        String fDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        //String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=" + strzip + "&date=" + fDate + "&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
+        String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=23185&date=2015-10-12&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
         Toast toast = Toast.makeText(getApplicationContext(), Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString(), Toast.LENGTH_SHORT);
         toast.show();
 
-        InputStream input = null;
-        OutputStream output = null;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         try {
-            URL url = new URL(strurl);
-            url.getQuery();
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                Toast toast1 = Toast.makeText(getApplicationContext(), "connected", Toast.LENGTH_SHORT);
-                toast1.show();
-            }
-            input = connection.getInputStream();
-            output = new FileOutputStream("/sdcard/file_name.extension");
-
-            int fileLength = connection.getContentLength();
-
-            byte data[] = new byte[4096];
-            long total = 0;
-            int count;
-            while ((count = input.read(data)) != -1) {
-                // allow canceling with back button
-
-                total += count;
-                // publishing the progress....
-                if (fileLength > 0) // only if total length is known
-                    publishProgress((int) (total * 100 / fileLength));
-                output.write(data, 0, count);
-            }
+            xmlresponse = sendGet(strurl);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (output != null)
-                    output.close();
-                if (input != null)
-                    input.close();
-            } catch (IOException ignored) {
-            }
-
-            if (connection != null)
-                connection.disconnect();
         }
-        } catch (MalformedURLException e) {
+        xmlshow.setText(xmlresponse);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new InputSource(new StringReader(xmlresponse)));
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("ForecastByZip");
+
+            for (int zip = 0; zip < nList.getLength(); zip++) {
+                Node nNode = nList.item(zip);
+                    Element eElement = (Element) nNode;
+                    xmlshow.append("DateIssue : " + eElement.getElementsByTagName("DateIssue").item(0).getTextContent()+"\n");
+                    xmlshow.append("DateForecast : " + eElement.getElementsByTagName("DateForecast").item(0).getTextContent()+"\n");
+                    xmlshow.append("Reporting Area : " + eElement.getElementsByTagName("ReportingArea").item(0).getTextContent()+"\n");
+                    xmlshow.append("Parameter Name : " + eElement.getElementsByTagName("ParameterName").item(0).getTextContent()+"\n");
+                    xmlshow.append("AQI : " + eElement.getElementsByTagName("AQI").item(0).getTextContent()+"\n");
+                    xmlshow.append("CategoryNumber : " + eElement.getElementsByTagName("CategoryNumber").item(0).getTextContent()+"\n");
+                    xmlshow.append("CategoryName : " + eElement.getElementsByTagName("CategoryName").item(0).getTextContent()+"\n");
+                    xmlshow.append("Actionday : " + eElement.getElementsByTagName("ActionDay").item(0).getTextContent()+"\n");
+                    //String resp = eElement.getTextContent();
+                    //xmlshow.setText(resp);
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-//        Toast toast = Toast.makeText(getApplicationContext(), Integer.toString(zipcode), Toast.LENGTH_SHORT);
-//        toast.show();
-//        toast.show();
-//        toast.show();
-
-
     }
 
-    //public int xmlparse(int zipcode, XmlDom xml, )
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_result, menu);
-        return true;
-    }
+    private String sendGet(String strurl) throws Exception {
+        String USER_AGENT = "Mozilla/5.0";
+        String url = strurl;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
+        in.close();
 
-        return super.onOptionsItemSelected(item);
+        //print result
+        Toast toast = Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT);
+        toast.show();
+
+        return response.toString();
     }
+
+
+
+
+
 }
