ad3ff258475b62585f7fa6cc489b7a6a61f3d182
indigowhale33 <motal90@hotmail.com>
2015-10-20 11:38:23 -0400

    successful xml downloading/parsing showing edittext its content

--- app/src/main/java/com/kevinandsteve/openwindow/ResultActivity.java
+++ app/src/main/java/com/kevinandsteve/openwindow/ResultActivity.java
@@ -1,87 +1,113 @@
 package com.kevinandsteve.openwindow;
 
-import android.app.ProgressDialog;
+import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class ResultActivity extends AppCompatActivity {
     //progress dialog
-    private ProgressDialog pDialog;
-    public static final int progress_bar_type = 0;
     private static String xmlresponse;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result2);
+        setContentView(R.layout.result_page);
         EditText xmlshow = (EditText) findViewById(R.id.resulttext);
 
         Intent extras = getIntent();
         int zipcode = -1;
-        if(extras != null){
+        if (extras != null) {
             zipcode = extras.getIntExtra("EXTRA_ZIPCODE", 0);
         }
         String strzip = Integer.toString(zipcode);
         String fDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        //String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=" + strzip + "&date=" + fDate + "&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
-        String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=23185&date=2015-10-12&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
+        String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=" + strzip + "&date=" + fDate + "&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
+        //String strurl = "http://www.airnowapi.org/aq/forecast/zipCode/?format=application/xml&zipCode=23185&date=2015-10-12&distance=20&API_KEY=89073F7F-3417-4795-B2CE-D9CF7FA83279";
         Toast toast = Toast.makeText(getApplicationContext(), Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString(), Toast.LENGTH_SHORT);
         toast.show();
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        new ProgressTask(strurl).execute();
+        try {
+            xmlresponse = sendGet(strurl);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-//        try {
-//            xmlresponse = sendGet(strurl);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        xmlshow.setText(xmlresponse);
-//
-//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//        DocumentBuilder builder = null;
-//        try {
-//            builder = factory.newDocumentBuilder();
-//            Document doc = builder.parse(new InputSource(new StringReader(xmlresponse)));
-//            doc.getDocumentElement().normalize();
-//            NodeList nList = doc.getElementsByTagName("ForecastByZip");
-//
-//            for (int zip = 0; zip < nList.getLength(); zip++) {
-//                Node nNode = nList.item(zip);
-//                    Element eElement = (Element) nNode;
-//                    xmlshow.append("DateIssue : " + eElement.getElementsByTagName("DateIssue").item(0).getTextContent()+"\n");
-//                    xmlshow.append("DateForecast : " + eElement.getElementsByTagName("DateForecast").item(0).getTextContent()+"\n");
-//                    xmlshow.append("Reporting Area : " + eElement.getElementsByTagName("ReportingArea").item(0).getTextContent()+"\n");
-//                    xmlshow.append("Parameter Name : " + eElement.getElementsByTagName("ParameterName").item(0).getTextContent()+"\n");
-//                    xmlshow.append("AQI : " + eElement.getElementsByTagName("AQI").item(0).getTextContent()+"\n");
-//                    xmlshow.append("CategoryNumber : " + eElement.getElementsByTagName("CategoryNumber").item(0).getTextContent()+"\n");
-//                    xmlshow.append("CategoryName : " + eElement.getElementsByTagName("CategoryName").item(0).getTextContent()+"\n");
-//                    xmlshow.append("Actionday : " + eElement.getElementsByTagName("ActionDay").item(0).getTextContent()+"\n");
-//                    //String resp = eElement.getTextContent();
-//                    //xmlshow.setText(resp);
-//            }
-//        } catch (ParserConfigurationException e) {
-//            e.printStackTrace();
-//        } catch (SAXException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        xmlshow.setText(xmlresponse);
+        Toast toast2 = Toast.makeText(getApplicationContext(), xmlresponse, Toast.LENGTH_SHORT);
+        toast2.show();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        String resp = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new InputSource(new StringReader(xmlresponse)));
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("ForecastByZip");
+            if (nList.getLength() != 0) {
+                for (int zip = 0; zip < nList.getLength(); zip++) {
+                    Node nNode = nList.item(zip);
+                    Element eElement = (Element) nNode;
+                    xmlshow.append("DateIssue : " + eElement.getElementsByTagName("DateIssue").item(0).getTextContent() + "\n");
+                    xmlshow.append("DateForecast : " + eElement.getElementsByTagName("DateForecast").item(0).getTextContent() + "\n");
+                    xmlshow.append("Reporting Area : " + eElement.getElementsByTagName("ReportingArea").item(0).getTextContent() + "\n");
+                    xmlshow.append("Parameter Name : " + eElement.getElementsByTagName("ParameterName").item(0).getTextContent() + "\n");
+                    xmlshow.append("AQI : " + eElement.getElementsByTagName("AQI").item(0).getTextContent() + "\n");
+                    xmlshow.append("CategoryNumber : " + eElement.getElementsByTagName("CategoryNumber").item(0).getTextContent() + "\n");
+                    xmlshow.append("CategoryName : " + eElement.getElementsByTagName("CategoryName").item(0).getTextContent() + "\n");
+                    xmlshow.append("Actionday : " + eElement.getElementsByTagName("ActionDay").item(0).getTextContent() + "\n");
+                    resp = eElement.getTextContent();
+                    xmlshow.append(resp);
+                    xmlshow.append(Integer.toString(nList.getLength()));
+                }
+            } else {
+                resp = "\nInvalid Zipcode / Try nearby zipcode";
+                xmlshow.append(resp);
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
@@ -122,44 +148,6 @@ public class ResultActivity extends AppCompatActivity {
     }
 
 
-
-    class ProgressTask extends AsyncTask<Void, Void, Void> {
-        ProgressDialog pd;
-        String strurl;
-        String xmlresponse;
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pd = new ProgressDialog(ResultActivity.this);
-            pd.setMessage("loading");
-            pd.show();
-        }
-
-        public ProgressTask(String strurl) {
-            super();
-            this.strurl = strurl;
-            // do stuff
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                this.xmlresponse = sendGet(strurl);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            if (pd != null)
-            {
-                pd.dismiss();
-            }
-        }
-    }
 
 
 
