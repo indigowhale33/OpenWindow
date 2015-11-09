ca8463a4771b8fff4d9bdd9d7c5a962abe8a7968
indigowhale33 <motal90@hotmail.com>
2015-11-09 11:06:23 -0500

    commit

---
+++ app/src/main/java/com/kevinandsteve/openwindow/Others.java
@@ -0,0 +1,7 @@
+package com.kevinandsteve.openwindow;
+
+/**
+ * Created by Steve on 2015-11-08.
+ */
+public class Others {
+}
---
+++ app/src/main/java/com/kevinandsteve/openwindow/TimeOtherFragment.java
@@ -0,0 +1,41 @@
+package com.kevinandsteve.openwindow;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Steve on 2015-10-29.
+ */
+public class TimeOtherFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        //Use the current time as the default values for the time picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        //Create and return a new instance of TimePickerDialog
+        return new TimePickerDialog(getActivity(),this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    //onTimeSet() callback method
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+        //Do something with the user chosen time
+        //Get reference of host activity (XML Layout File) TextView widget
+        TextView tv = (TextView) getActivity().findViewById(R.id.textv);
+        //Set a message for user
+        //tv.setText("Your chosen time is...");
+        //Display the user changed time on TextView
+        tv.setText("You will be notified daily at "+ String.valueOf(hourOfDay)+" : "
+                + String.valueOf(minute));
+    }
+}
